a3b81c25460d33d425ec6c78eda4bd600679b777
Otavio Desktop <otavio_quilice@hotmail.com>
2025-10-01 19:58:51 -0300

    feat: :sparkles: Adicionando select e mock de caminhões

--- src/pages/Monitoramento/index.jsx
+++ src/pages/Monitoramento/index.jsx
@@ -1,4 +1,6 @@
+import { useState } from "react"
 import styled from "styled-components"
+import SelectCaminhao from "../../components/SelectCaminhao"
 import ItemListaEntregas from "../../components/ItemListaEntregas"
 
 const ContainerStyled = styled.div`
@@ -24,17 +26,39 @@ const SectionListaEntregasStyled = styled.section`
 
 
 const Monitoramento = () => {
+    const [caminhaoSelecionado, setCaminhaoSelecionado] = useState("")
+
+    const caminhoes = ["Caminhão 1", "Caminhão 2", "Caminhão 3"]
+
+    const entregas = [
+        { id: 1, caminhao: "Caminhão 1", tipo: "Congelados" },
+        { id: 2, caminhao: "Caminhão 2", tipo: "Congelados" },
+        { id: 3, caminhao: "Caminhão 1", tipo: "Congelados" },
+        { id: 4, caminhao: "Caminhão 3", tipo: "Congelados" },
+    ]
+
+    const entregasFiltradas = caminhaoSelecionado
+        ? entregas.filter(entrega => entrega.caminhao === caminhaoSelecionado)
+        : entregas
+
     return (
         <ContainerStyled className="container">
             <ConteudoPrincipalStyled>
                 <h2>Página monitoramento</h2>
+                <select onChange={(e) => setCaminhaoSelecionado(e.target.value)}>
+                    <option value="">Todos os caminhões</option>
+                    {caminhoes.map((caminhao, index) => (
+                        <option key={index} value={caminhao}>{caminhao}</option>
+                    ))}
+                </select>
                 <SectionListaEntregasStyled>
-                    <ItemListaEntregas />
-                    <ItemListaEntregas />
-                    <ItemListaEntregas />
-                    <ItemListaEntregas />
-                    <ItemListaEntregas />
-                    <ItemListaEntregas />
+                    {entregasFiltradas.map(entrega => (
+                        <ItemListaEntregas
+                            key={entrega.id}
+                            caminhao={entrega.caminhao}
+                            tipo={entrega.tipo}
+                        />
+                    ))}
                 </SectionListaEntregasStyled>
             </ConteudoPrincipalStyled>
         </ContainerStyled>
